9bda78bf460bcd057ffdbc7731bf38ca9f08a47e
Jonaskjeld <32664553+Jonaskjeld@users.noreply.github.com>
2017-10-10 13:58:58 +0200

    Update DisplayHandler.java

--- Project1Calculatos/src/calculatorPackage/DisplayHandler.java
+++ Project1Calculatos/src/calculatorPackage/DisplayHandler.java
@@ -2,5 +2,17 @@ package calculatorPackage;
 
 public class DisplayHandler
 {
+  // Declare a Text call it textdisplayed
+  
+  // Declare method: UpdateScreen(List<inputnodes>)
+      // 
+  
+  // Declare method: UpdateScreen(Result)
+     // return result as a string. 
+       // if result is valid
+          // print result
+       // else if result isn't valid
+          // print "invalid result"
+  
 
 }
