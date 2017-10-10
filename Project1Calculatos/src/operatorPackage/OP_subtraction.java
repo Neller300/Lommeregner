20bc2a5f5983b27e82fa6b562d830ea78a9f2389
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-10 13:45:08 +0200

    Update OP_subtraction.java

--- Project1Calculatos/src/operatorPackage/OP_subtraction.java
+++ Project1Calculatos/src/operatorPackage/OP_subtraction.java
@@ -1,6 +1,7 @@
 package operatorPackage;
 
-public class OP_subtraction
+public class OP_subtraction Extends OperatorMaster
 {
-
+//call calc method(?,?)
+ //final priority - used to determine when it's being used in calc method.
 }
