5a68537ec91b7a1401470c3ba26289d31ca95957
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-10 13:44:54 +0200

    Update OP_squared.java

--- Project1Calculatos/src/operatorPackage/OP_squared.java
+++ Project1Calculatos/src/operatorPackage/OP_squared.java
@@ -1,6 +1,7 @@
 package operatorPackage;
 
-public class OP_squared
+public class OP_squared Extends OperatorMaster
 {
-
+//call calc method(?,?)
+ //final priority - used to determine when it's being used in calc method.
 }
