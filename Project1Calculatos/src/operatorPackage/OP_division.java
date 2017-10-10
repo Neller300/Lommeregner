b2d4e2ad1a8c152f06243e474a87d81763b52553
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-10 13:43:56 +0200

    Update OP_division.java

--- Project1Calculatos/src/operatorPackage/OP_division.java
+++ Project1Calculatos/src/operatorPackage/OP_division.java
@@ -1,6 +1,8 @@
 package operatorPackage;
 
-public class OP_division
+public class OP_division Extends OperatorMaster
 {
+  //call calc method(?,?)
+ //final priority - used to determine when it's being used in calc method.
 
 }
