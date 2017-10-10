320ea54fbbd9a82178ffc837443b3aef02781889
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-10 13:43:38 +0200

    Update OP_addition.java

--- Project1Calculatos/src/operatorPackage/OP_addition.java
+++ Project1Calculatos/src/operatorPackage/OP_addition.java
@@ -1,7 +1,8 @@
 package operatorPackage;
 
-public class OP_addition
+public class OP_addition extends OperatorMaster
 {
-  
+ //call calc method(?,?)
+ //final priority - used to determine when it's being used in calc method.
 
 }
