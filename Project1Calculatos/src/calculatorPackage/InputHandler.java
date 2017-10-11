1f4a5cc22ec4434b6eb321b15e6290a2c2e7f571
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-11 13:47:12 +0200

    Update InputHandler.java

--- Project1Calculatos/src/calculatorPackage/InputHandler.java
+++ Project1Calculatos/src/calculatorPackage/InputHandler.java
@@ -2,5 +2,66 @@ package calculatorPackage;
 
 public class InputHandler
 {
+  //Branching
+  //if number
+    //if(trackedinput.empty || trackedinput.isoperator
+      //create node, add to last
+  //else
+    //append to nodes input
+  
+  //if input == "."
+    //if(currentnode.input.contains(".")
+      //return;
+  
+  //if(trackedinput.isempty ||trackedinput.isoperator)
+    //create new node and use input "0."
+  
+  //else
+    //append to nodes input
+  
+  //if operator
+    //if(trackedinput.isEmpty
+      //create node add to list
+  //else
+    //return
+  
+  //if(currentnode.isNumber)
+    //create node and add to list
+  
+  //if(currentnode.isOperator)
+    //if(currentnode.direction==l || direction==m)
+      //create node and add to list
+    
+    //if(currentnode.direction==m || direction==r)
+      //create node and add to list 
+  
+  //void changeSignPressed()
+    //if(trackedinput.isempty || currentnode.isOperator)
+      //create node and add to list
+    //if(currentnode.isNumber)
+      //prepend "-" to current input
+  
+  //void clear()
+    //trackedinput.clear()
+    //updateDisplay(" ")
+  
+  //void delete
+    //if(trackedinput.isEmpty
+      //return
+    //if(currentnodeinput<=1)
+      //remove node from trackedlist
+    //else removeCharAt(input.length-1)
+  
+  //void enterPressed()
+    //if(trackedinput.isEmpty)
+      //return;
+    //else
+      //list<inputnodes> templist = new Arraylist<inputnode>();
+      //foreach oldnode in trackedlist
+        //inputnode newcopy = oldnode.copy();
+        //add newcopy to template
+          //display.show(calcresultfinal(calcParenthesis(templist)));
+  
+  
 
 }
