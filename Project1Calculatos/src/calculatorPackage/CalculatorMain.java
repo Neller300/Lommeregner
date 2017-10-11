f4ad3c266800e3d5036459f92ff7e569835cf080
Jonaskjeld <32664553+Jonaskjeld@users.noreply.github.com>
2017-10-11 09:52:06 +0200

    Update CalculatorMain.java

--- Project1Calculatos/src/calculatorPackage/CalculatorMain.java
+++ Project1Calculatos/src/calculatorPackage/CalculatorMain.java
@@ -5,7 +5,11 @@ public class CalculatorMain
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		// Create new instance GUICreator. Call it gui
+		// Create new instance DisplayHandler. Call it display
+		// Create new instance CalcResult. Call it thecalculator
+		// Create new instance InputHandler. Call it ihandler
+		
 
 	}
 
