b86fbf70f28a401838029af20210940392cbf7ef
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-11 09:47:39 +0200

    Update DisplayHandler.java

--- Project1Calculatos/src/calculatorPackage/DisplayHandler.java
+++ Project1Calculatos/src/calculatorPackage/DisplayHandler.java
@@ -5,7 +5,7 @@ public class DisplayHandler
   // Declare a Text call it textdisplayed
   
   // Declare method: UpdateScreen(List<inputnodes>)
-      // 
+      // for each loop prints input nodes
   
   // Declare method: UpdateScreen(Result)
      // return result as a string. 
