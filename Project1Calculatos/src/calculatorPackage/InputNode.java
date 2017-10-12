8ea7df8a9f43c72fb6ce58988b4753b625fe6eb9
hkkrestless <32671411+hkkrestless@users.noreply.github.com>
2017-10-12 08:32:31 +0200

    Update InputNode.java

--- Project1Calculatos/src/calculatorPackage/InputNode.java
+++ Project1Calculatos/src/calculatorPackage/InputNode.java
@@ -2,5 +2,14 @@ package calculatorPackage;
 
 public class InputNode
 {
-
+//Create variables 
+	//string input - the input used for calculations
+	//int priority - priority used for precedence calculation
+	//inputNode left - its left neighbor
+	//inputNode Right - its right neighbor
+	//boolean isOperator - true if operator
+	
+	//create constructor taking arguments to fill instance variables
+	
+	//make copyconstructor
 }
