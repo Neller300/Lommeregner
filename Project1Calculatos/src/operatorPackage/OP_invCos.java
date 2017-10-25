3d279cab299d6346bc37649d1614c301d4596489
Neller300 <32664509+Neller300@users.noreply.github.com>
2017-10-25 09:20:24 +0200

    Add files via upload

---
+++ Project1Calculatos/src/operatorPackage/OP_invCos.java
@@ -0,0 +1,18 @@
+package operatorPackage;
+
+public class OP_invCos extends OperatorMaster
+{
+	private static final int priority=1;
+	
+	
+	public Double calc(Double par1, Double par2)
+	{
+		return Math.toDegrees(Math.acos(par2));
+	}
+	
+
+	public int getPriority()
+	{
+		return priority;
+	}
+}
