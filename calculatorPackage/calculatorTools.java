3aee8befb6c5eab2870f8f51ad85f56c29e96348
Jonaskjeld <32664553+Jonaskjeld@users.noreply.github.com>
2017-10-27 09:38:37 +0200

    Delete calculatorTools.java

--- calculatorPackage/calculatorTools.java
+++
@@ -1,8 +0,0 @@
-package calculatorPackage;
-
-public class calculatorTools
-{
-	public static enum nodeType {OPERATOR, NUMBER, PARENTHESIS, UTILITY};
-	
-	public static enum displayType {CALCULATOR, Y1, Y2, Y3};
-}
